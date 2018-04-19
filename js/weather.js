a2992fe3854d92b497ed549068875dcb3e00dacd
Bengu Erenler <Bengu.Erenler.6565@student.uu.se>
2018-04-19 13:52:24 +0200

    changing into open weather api

--- js/weather.js
+++ js/weather.js
@@ -2,29 +2,42 @@
  * Created by benguerenler on 13.04.2018.
  */
 
-var request = new XMLHttpRequest();
-request.open('GET', '/api/location/search/?query=(query)', true);
-request.onload = function () {
-    // Begin accessing JSON data here
-}
-// Send request
-request.send();
-
-var data = JSON.parse(this.response);
-
-/**
-data.forEach(movie => {
-    // Log each movie's title
-    console.log(movie.title);
-});
- **/
 
 function check_form() {
-    document.getElementById("search").innerHTML = "";
+    //document.getElementById("search").innerHTML = "";
 
     //Get the input and store it in the var
     var search = document.getElementById("searchInput").value;
-    
+    gettingJSON();
     //TO:DO use the search for the get the weather with API
+}
 
+function gettingJSON(){
+  //  document.write("jquery loaded");
+    $.getJSON("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=4ba63d16825755a279cabc0a923b7b0d&units=metric",function(json){
+       // document.write(JSON.stringify(json.main.temp));
+        //send the data json to new function
+        gotData(json);
+    });
+}
+
+function gotData(data) {
+
+    var weather = data;
+    var weathers = [];
+    for (var i =0 ; i<40;)
+    {
+        weathers.push({
+            temperature: weather.list[i].main.temp,
+            text: weather.list[i].weather[0].description,
+            date: weather.list[i].dt_txt}
+    )
+     //   document.write( weathers[i].text.toString());
+        i = i + 7;
+    }
+
+      document.write( weathers[3].date.toString());
+
+
+    return weathers;
 }
